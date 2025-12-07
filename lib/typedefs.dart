523f328f1fa05d7544fbe36a4c56cee92094e2f7
Chris Norman <chris.norman1989@outlook.com>
2025-12-07 13:32:29 +0000

    More doc comments.

--- lib/typedefs.dart
+++ lib/typedefs.dart
@@ -1,3 +1,6 @@
+/// Various type definitions used throughout the engine.
+library;
+
 import 'package:angstrom/angstrom.dart';
 
 /// The type of a function which is called with an angstrom engine as its only
